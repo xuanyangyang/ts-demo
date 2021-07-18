998f45362221439c0c7132541bb7069c5b55624a
xuanyangyang <lierabbit@qq.com>
2021-07-19 02:34:21 +0800

    调整顺序

--- src/Index.ts
+++ src/Index.ts
@@ -4,6 +4,6 @@
  * @author xuanyangyang
  * @since 1.0
  */
+export {TSObject3} from "./TSObject3.js";
 export {TSObject1} from "./TSObject1.js";
 export {TSObject2} from "./TSObject2.js";
-export {TSObject3} from "./TSObject3.js";
