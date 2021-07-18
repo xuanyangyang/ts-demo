a77c8230bb158d66479fa8fe94a4770fb39ecb1d
xuanyangyang <lierabbit@qq.com>
2021-07-19 02:45:40 +0800

    ts5

--- src/Test.ts
+++ src/Test.ts
@@ -4,9 +4,7 @@
  * @author xuanyangyang
  * @since 1.0
  */
-import {TSObject2, TSObject4} from "./Index.js";
-import {TSObject1} from "./Index.js";
-import {TSObject3} from "./Index.js";
+import {TSObject1, TSObject2, TSObject3, TSObject4, TSObject5} from "./Index.js";
 
 let t1 = new TSObject1('1');
 let t2 = new TSObject2(2);
@@ -14,5 +12,7 @@ console.log(t1);
 console.log(t2);
 let t3 = new TSObject3(t1, t2);
 console.log(t3);
-let t4 = new TSObject4(4,"4");
+let t4 = new TSObject4(4, "4");
 console.log(t4);
+let t5 = new TSObject5(t1);
+console.log(t5);
